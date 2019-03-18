d3696655acd42ace47e27d0946d4c3c3bef21161
truongnt <ntt261298@gmail.com>
2019-03-18 14:05:04 +0700

    Add migration db

--- run.py
+++ run.py
@@ -1,17 +1,4 @@
 from main import app
-import main.controllers
-from main.authenticate.security import bcrypt
-from database.db import db
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
-db.init_app(app)
-bcrypt.init_app(app)
-
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, threaded=True, host='0.0.0.0')
