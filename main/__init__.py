57741a8a6364052302c07daccbd5b4c3b7c15c4a
truongnt <ntt261298@gmail.com>
2019-03-18 15:40:02 +0700

    FIX: can't import app

--- main/__init__.py
+++ main/__init__.py
@@ -1,20 +1,9 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-import main.controllers
-from main.authenticate.security import bcrypt
-from database.db import db
 
 app = Flask(__name__)
 
+jwt = JWTManager(app)
+
 # Configurations
 app.config.from_object('config')
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
-jwt = JWTManager(app)
-db.init_app(app)
-bcrypt.init_app(app)
--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 from main import app
+from main.authenticate.security import bcrypt
+from database.db import db
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+db.init_app(app)
+bcrypt.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, threaded=True, host='0.0.0.0')
