03361ed25d9e23034dcba09905ab82a25efdf36a
truongnt <ntt261298@gmail.com>
2019-03-18 11:18:09 +0700

    config mysql database

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "data.db")
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/catalog-api'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 
